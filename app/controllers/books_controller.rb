26265986047797007fbdf3c2a0d696b26a3b3a1f
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-07-28 13:50:17 -0500

    Set books controller new and create actions with strong parameters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,5 +7,23 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new book_params
+
+    if @book.save
+      flash[:success] = "Saved!"
+      redirect_to root_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:name, :author)
+    end
 end
