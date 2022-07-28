86c6b0657671d79563c93ca9c18864f4aa8b0eb1
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-07-28 12:11:19 -0500

    Set presence validations for Book columns, and run migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-  
+  validates :name, presence: true
+  validates :author, presence: true
 end
--- db/migrate/20220728165853_create_books.rb
+++ db/migrate/20220728165853_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :name
-      t.string :author
+      t.string :name, null: false
+      t.string :author, null: false
 
       t.timestamps
     end
